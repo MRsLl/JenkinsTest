15ee393faf32eaf2c80ea73ea3cfe9e90bf314ed
MRsLi <17861509590@163.com>
2021-01-12 20:19:16 +0800

    测试自动化部署

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
       <h2>测试手动部署</h2>
       <h2>测试脚本部署</h2>
+      <h2>测试自动化部署</h2>
    <img src="https://image.baidu.com/search/detail?ct=503316480&z=undefined&tn=baiduimagedetail&ipn=d&word=%E5%9B%9B%E8%B7%AF%E6%B3%B0%E5%9D%A6&step_word=&ie=utf-8&in=&cl=2&lm=-1&st=undefined&hd=undefined&latest=undefined&copyright=undefined&cs=4218553786,2820097030&os=3709420409,332224811&simid=4073557387,413682981&pn=0&rn=1&di=1760&ln=482&fr=&fmq=1610448641589_R&fm=&ic=undefined&s=undefined&se=&sme=&tab=0&width=undefined&height=undefined&face=undefined&is=0,0&istype=0&ist=&jit=&bdtype=0&spn=0&pi=0&gsm=0&objurl=https%3A%2F%2Fgimg2.baidu.com%2Fimage_search%2Fsrc%3Dhttp%253A%252F%252F5b0988e595225.cdn.sohucs.com%252Fq_70%252Cc_zoom%252Cw_640%252Fimages%252F20190110%252Fc3ab939df555499b942d47990dfeb4f8.jpeg%26refer%3Dhttp%253A%252F%252F5b0988e595225.cdn.sohucs.com%26app%3D2002%26size%3Df9999%2C10000%26q%3Da80%26n%3D0%26g%3D0n%26fmt%3Djpeg%3Fsec%3D1613040641%26t%3D4e3cb9c6ca321540dbc022e5573a9107&rpstart=0&rpnum=0&adpicid=0&force=undefined&ctd=1610448645812^3_1519X792%1"/>
   </div>
 </template>
